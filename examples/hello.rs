61cfe11edd4a0c88a1d92c715acc981d5576be09
Alessandro Pellizzari <alex@amiran.it>
2018-02-25 13:33:33 +0000

    Initial version

---
+++ examples/hello.rs
@@ -0,0 +1,5 @@
+#!/usr/bin/env runst
+
+fn main() {
+    println!("Hello world");
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#[macro_use]
+extern crate failure;
+
+use std::env;
+use std::process::{self, Command, Stdio};
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter};
+use std::fs::{self, File};
+
+use failure::Error;
+
+fn main() {
+    let args = env::args().skip(1).collect::<Vec<String>>();
+    if args.is_empty() {
+        eprintln!("usage: runst <source file> [...]");
+        process::exit(1);
+    }
+
+    let _ = run(args);
+}
+
+fn run(args: Vec<String>) -> Result<(), Error> {
+    let metadata = fs::metadata(&args[0])?;
+    if !metadata.file_type().is_file() {
+        return Err(RuntimeError::NotRegular(args[0].to_string()).into());
+    }
+
+    let mut source = String::with_capacity(metadata.len() as usize);
+    BufReader::new(File::open(&args[0])?).read_to_string(&mut source)?;
+
+    let target = "/tmp/runsting";
+
+    // rustc --crate-name runst --crate-type bin --emit=link -C opt-level=3  --out-dir ./ -
+    let mut cmd = Command::new("rustc")
+        .stdin(Stdio::piped())
+        .args(&[
+            "--crate-name",
+            "runst",
+            "--crate-type",
+            "bin",
+            "--emit=link",
+            "-C",
+            "opt-level=3",
+            "-o",
+            target,
+            "-",
+        ])
+        .spawn()?;
+
+    {
+        let mut writer = BufWriter::new(cmd.stdin.take().unwrap());
+        writer.write_all(source.as_bytes())?;
+    }
+
+
+    let compiler_result = cmd.wait_with_output()?;
+
+    if !compiler_result.status.success() {
+        return Err(RuntimeError::CompilerError(
+            compiler_result.status.code().unwrap_or(0),
+            String::from_utf8_lossy(&compiler_result.stderr).to_string(),
+        ).into());
+    }
+
+    Command::new(target)
+        .spawn()?;
+
+    Ok(())
+}
+
+#[derive(Debug, Fail)]
+enum RuntimeError {
+    #[fail(display = "not a regular file: {}", _0)] NotRegular(String),
+
+    #[fail(display = "compiler failed with status {}:\n{}", _0, _1)] CompilerError(i32, String),
+}
